2d972b58362042ce6c1e009c181f6ba58f5a8531
Alexandre Lamarre <alex7285@gmail.com>
2024-04-24 13:01:17 -0400

    (feat) add accumulate functions
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

---
+++ sequences.go
@@ -0,0 +1,56 @@
+package ro
+
+import (
+	"iter"
+)
+
+// Accumulate returns an iterator that yields the accumulated sum of the elements in seq
+func Accumulate[i intType](seq []i) iter.Seq[i] {
+	return func(yield func(i) bool) {
+		sum := i(0)
+		for _, v := range seq {
+			sum += v
+			if !yield(sum) {
+				break
+			}
+		}
+	}
+}
+
+// AccumulateIter returns an iterator that yields the accumulated sum of the elements yielded by seq
+func AccumulateIter[i intType](seq iter.Seq[i]) iter.Seq[i] {
+	return func(yield func(i) bool) {
+		sum := i(0)
+		for v := range seq {
+			sum += v
+			if !yield(sum) {
+				break
+			}
+		}
+	}
+}
+
+// AccumulateFunc returns an iterator that yields the accumulated result of applying f to the elements in seq
+func AccumulateFunc[T any](seq []T, f func(T, T) T) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		var agg T
+		for _, v := range seq {
+			agg = f(agg, v)
+			if !yield(agg) {
+				break
+			}
+		}
+	}
+}
+
+func AccumulateIterFunc[T any](seq iter.Seq[T], f func(T, T) T) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		var agg T
+		for v := range seq {
+			agg = f(agg, v)
+			if !yield(agg) {
+				break
+			}
+		}
+	}
+}
---
+++ sequences_test.go
@@ -0,0 +1,67 @@
+package ro_test
+
+import (
+	"testing"
+
+	"github.com/alexandreLamarre/ro"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccumulate(t *testing.T) {
+	seq := []int{1, 2, 3, 4}
+	res := []int{}
+	for v := range ro.Accumulate(seq) {
+		res = append(res, v)
+	}
+
+	assert.Equal(t, []int{1, 3, 6, 10}, res)
+
+	empty := []int{}
+	res2 := []int{}
+	for v := range ro.Accumulate(empty) {
+		res2 = append(res2, v)
+	}
+	assert.Equal(t, []int{}, res2)
+}
+
+func TestAccumulateIter(t *testing.T) {
+	res := []int{}
+	for v := range ro.AccumulateIter(ro.SeqAsIter([]int{1, 2, 3, 4})) {
+		res = append(res, v)
+	}
+	assert.Equal(t, []int{1, 3, 6, 10}, res)
+
+	res2 := []int{}
+	for v := range ro.AccumulateIter(ro.SeqAsIter([]int{})) {
+		res2 = append(res2, v)
+	}
+	assert.Equal(t, []int{}, res2)
+}
+
+func TestAccumulateFunc(t *testing.T) {
+	res := [][]string{}
+	for v := range ro.AccumulateFunc([][]string{{"a", "b"}, {"c", "d"}}, func(a, b []string) []string { return append(a, b...) }) {
+		res = append(res, v)
+	}
+	assert.Equal(t, [][]string{{"a", "b"}, {"a", "b", "c", "d"}}, res)
+
+	res2 := []int{}
+	for v := range ro.AccumulateFunc([]int{}, func(a, b int) int { return a % b }) {
+		res2 = append(res2, v)
+	}
+	assert.Equal(t, []int{}, res2)
+}
+
+func TestAccumulateIterFunc(t *testing.T) {
+	res := [][]string{}
+	for v := range ro.AccumulateIterFunc(ro.SeqAsIter([][]string{{"a", "b"}, {"c", "d"}}), func(a, b []string) []string { return append(a, b...) }) {
+		res = append(res, v)
+	}
+	assert.Equal(t, [][]string{{"a", "b"}, {"a", "b", "c", "d"}}, res)
+
+	res2 := []int{}
+	for v := range ro.AccumulateIterFunc(ro.SeqAsIter([]int{}), func(a, b int) int { return a % b }) {
+		res2 = append(res2, v)
+	}
+	assert.Equal(t, []int{}, res2)
+}
