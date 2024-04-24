e187c2669587144368df08f79f488f422f30dec5
Alexandre Lamarre <alex7285@gmail.com>
2024-04-24 11:55:09 -0400

    (feat) introduce more generic number types
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

--- types.go
+++ types.go
@@ -10,3 +10,17 @@ type intType interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
+
+type floatType interface {
+	~float32 | ~float64
+}
+
+type complexType interface {
+	~complex64 | ~complex128
+}
+
+type numberType interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+	~float32 | ~float64 | ~complex64 | ~complex128
+}
