48863272c2b11ded8aa39fa95d669f60fb7ef03d
Alexandre Lamarre <alex7285@gmail.com>
2024-05-16 13:19:11 -0400

    wip, squash me somewhere
    Signed-off-by: Alexandre Lamarre <alex7285@gmail.com>

--- parallel/context.go
+++ parallel/context.go
@@ -9,6 +9,30 @@ import (
 	"github.com/alexandreLamarre/ro"
 )
 
+type ParallelOptions struct {
+	Ctx context.Context
+}
+
+func (o *ParallelOptions) Apply(opts ...ParallelOption) {
+	for _, opt := range opts {
+		opt(o)
+	}
+}
+
+func defaultParallelOptions() *ParallelOptions {
+	return &ParallelOptions{
+		Ctx: context.Background(),
+	}
+}
+
+type ParallelOption func(*ParallelOptions)
+
+func WithContext(ctx context.Context) ParallelOption {
+	return func(o *ParallelOptions) {
+		o.Ctx = ctx
+	}
+}
+
 // Context returns an iterator that embeds a context with another iterator
 func Context[T any](ctx context.Context, seq iter.Seq[T]) ro.SeqCtx[T] {
 	return func(yield func(context.Context, T) bool) {
@@ -31,4 +55,12 @@ func ApplyCtx[U, V any](
 	f func(context.Context, U) (V, error),
 ) ro.SeqErr[V] {
 	return nil
+}
+
+func Go[T any](seq iter.Seq[T], workers int, opts ...ParallelOption) iter.Seq[T] {
+	options := defaultParallelOptions()
+	options.Apply(opts...)
+	return func(yield func(T) bool) {
+
+	}
 }
